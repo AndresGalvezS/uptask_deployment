389b9594e6b928f5764b434db97168da23a58a5f
unknown <agalvezsedelmayer@gmail.com>
2020-08-04 21:00:17 -0500

    Arreglado

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.use('/', routes());
 
 //Servidor y Puerto
 const HOST = process.env.HOST || '0.0.0.0';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || '5000';
 
 app.listen(PORT, HOST, ()=>{
     console.log('El servidor esta funcionando.');
